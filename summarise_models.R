35c8133fc9e113886dbfdf5ae6f868bc364d2e9a
Anders Bjørn Møller <perserkongen@gmail.com>
2026-03-25 14:02:23 +0100

    Analyzed accuracy and uncertainty for LU1700

--- summarise_models.R
+++ summarise_models.R
@@ -9,6 +9,8 @@ library(future)
 library(caret)
 library(ranger)
 library(tibble)
+library(tidyr)
+library(tidyterra)
 
 dir_code <- getwd()
 root <- dirname(dir_code)
@@ -316,5 +318,182 @@ pred_test_ochre_presence <- predict(
 pred_test_ochre_presence
 
 twoClassSummary(pred_test_ochre_presence, lev = c("No", "Yes"))
+
+
+# Average importance for LU models
+
+LU_imp_all <- sapply(
+  c(1:length(LU_models)),
+  function(x) {
+    myimp <- varImp(LU_models[[x]]$model)
+    
+    out <- myimp$importance %>%
+      rownames_to_column() %>%
+      mutate(
+        target = names(LU_models)[x]
+      )
+    
+    return(out)
+  },
+  simplify = FALSE
+) %>%
+  bind_rows() %>%
+  as.data.frame()
+
+LU_imp_all %>%
+  pivot_wider(
+    values_from = Overall,
+    names_from = target
+  ) %>% 
+  mutate(
+    across(
+      everything(), 
+      ~replace_na(., 0)
+    )
+  ) %>%
+  rowwise() %>%
+  mutate(
+    mean = mean(
+      c(
+        Forest, Thicket, Moor, Meadow_or_bog, Lake, Open_country, Marsh, 
+        Market_town, Dunes, Sea
+      )
+    )
+  ) %>%
+  ungroup() %>%
+  arrange(-mean) %>%
+  select(rowname, mean) %>%
+  slice_head(n = 20) %>%
+  as.data.frame()
+
+
+# Independent validation for LU models
+
+LU1700_ROC_test <- sapply(
+  c(1:length(LU_models)),
+  function(x) {
+    
+    LU_data_test_i <- LU1700_data_test %>%
+      mutate(
+        is_lu = factor(
+          as.numeric(lu18thcent == x),
+          levels = c(0, 1),
+          labels = c("No", "Yes")
+        )
+      )
+    
+    pred_i <- predict(
+      LU_models[[x]]$model,
+      newdata = LU_data_test_i,
+      type = "prob"
+    ) %>%
+      mutate(
+        obs = LU_data_test_i$is_lu,
+        pred = predict(
+          LU_models[[x]]$model,
+          newdata = LU_data_test_i
+        )
+      )
+    
+    out <- twoClassSummary(pred_i, lev = c("No", "Yes"))
+    
+    return(out)
+  },
+  simplify = FALSE
+) %>%
+  bind_rows()
+
+LU1700_ROC_test
+
+# Accuracy for LU class predictions
+
+rast_LU_pred <- paste0(
+  dir_dat, "/predictions/LU1700_class.tif"
+) %>%
+  rast()
+
+LU_test_pred_class <- LU_1700_pts %>%
+  mutate(
+    fold = LU1700_data_all$fold
+  ) %>%
+  filter(fold == 10) %>%
+  terra::extract(
+    x = rast_LU_pred,
+    y = .,
+    ID = FALSE,
+    bind = TRUE
+  )
+
+LU_confusion <- LU_test_pred_class %>%
+  values() %>%
+  mutate(
+    pred = factor(
+      LU1700_class, 
+      levels = c(1:10), 
+      labels = LU_1700_summary$LU_txt_nospace
+      ),
+    obs = factor(
+      lu18thcent, 
+      levels = c(1:10), 
+      labels = LU_1700_summary$LU_txt_nospace
+    )
+  ) %>%
+  select(
+    obs, pred
+  ) %>%
+  as.list() %>%
+  table() %>%
+  confusionMatrix()
+
+LU_confusion
+
+LU_confusion$byClass
+
+LU_confusion$byClass %>% dimnames() %>% magrittr::extract2(2) %>% as.data.frame()
+
+
+# Compare to uncertainty
+
+rast_LU_unc <- paste0(
+  dir_dat, "/predictions/LU1700_uncertainty.tif"
+) %>%
+  rast()
+
+rast_LU_class_unc <- c(
+  rast_LU_pred,
+  rast_LU_unc
+)
+
+LU_test_class_unc <- LU_1700_pts %>%
+  mutate(
+    fold = LU1700_data_all$fold
+  ) %>%
+  filter(fold == 10) %>%
+  terra::extract(
+    x = rast_LU_class_unc,
+    y = .,
+    ID = FALSE,
+    bind = TRUE
+  ) %>%
+  values() %>%
+  mutate(
+    correct = as.numeric(lu18thcent == LU1700_class),
+    unc_class = cut(LU1700_uncertainty, seq(0, 1, 0.1), include.lowest = TRUE)
+  )
+
+LU_test_class_unc %>%
+  group_by(lu18thcent) %>%
+  summarise(mean_unc = mean(LU1700_uncertainty))
+
+LU_test_class_unc %>%
+  group_by(LU1700_class) %>%
+  summarise(mean_unc = mean(LU1700_uncertainty))
+
+LU_test_class_unc %>%
+  group_by(unc_class) %>%
+  summarise(
+    perc_correct = mean(correct)*100,
+    n = n()
+    )
 
 # END
